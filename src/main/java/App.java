e160a107bfbcd679e5836b0f5a8768f3a5672cdb
Vu Diep <Vu Diep>
2017-02-25 09:26:09 +0700

    init project

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+
+import controllers.Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
+}
