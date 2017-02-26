24fb17583bca1d4352be044c98b1a994a5bf8f41
Vu Diep <Vu Diep>
2017-02-26 19:06:52 +0700

    fix bug

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 import org.springframework.boot.SpringApplication;
 
-import controllers.Example;
+import controllers.HomeController;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ import controllers.Example;
  */
 public class App {
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Example.class, args);
+		SpringApplication.run(HomeController.class, args);
 	}
 }
--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -2,11 +2,13 @@ package controllers;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/")
+@EnableAutoConfiguration
 public class HomeController {
 	@RequestMapping(method = GET)
 	public String home() {
